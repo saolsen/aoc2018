64f34bbe7d5229b3a5536c7be4085aa240f87f18
saolsen <steve.olsen@gmail.com>
2018-12-01 13:40:24 -0500

    part 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -2,31 +2,39 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
+use std::collections::HashSet;
+
 // Doing it a low level way that's closer to how  I'll do it
 // in stee.
 fn main() -> std::io::Result<()> {
     let f = File::open("src/bin/day01.txt")?;
     let mut reader = BufReader::new(f);
-    
+    let mut input_buf = vec![];
+    reader.read_to_end(&mut input_buf)?;
+    let input = input_buf.split(|i| *i == '\n' as u8).collect::<Vec<&[u8]>>();
+
+    let mut seen_frequencies = HashSet::new();
     let mut frequency : i32 = 0;
-    for maybe_line in reader.split(b'\n') {
-        if let Ok(line) = maybe_line {
-            let mut n : i32 = 0;
-            for digit in &line[1..] {
-                n *= 10;
-                n += (*digit - '0' as u8) as i32;
-            }
-            let sign = line[0] as char;
-            match sign {
-                '+' => frequency += n,
-                '-' => frequency -= n,
-                _ => {}
+    'outer: loop {
+        for ref line in &input {
+            if line.len() > 0 {
+                let mut n : i32 = 0;
+                for digit in &line[1..] {
+                    n *= 10;
+                    n += (digit - '0' as u8) as i32;
+                }
+                let sign = line[0] as char;
+                if sign == '+' {
+                    frequency += n;
+                } else {
+                    frequency -= n;
+                }
+                if !seen_frequencies.insert(frequency) {
+                    break 'outer;
+                }
             }
         }
     }
-    println!("{}", frequency);
-
+    println!("First Repeat: {}", frequency);
     Ok(())
 }
-
-
