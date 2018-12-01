16f464b7157f6b7639def2a5d625b547f206bce2
saolsen <steve.olsen@gmail.com>
2018-12-01 12:29:29 -0500

    part 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello World");
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+// Doing it a low level way that's closer to how  I'll do it
+// in stee.
+fn main() -> std::io::Result<()> {
+    let f = File::open("src/bin/day01.txt")?;
+    let mut reader = BufReader::new(f);
+    
+    let mut frequency : i32 = 0;
+    for maybe_line in reader.split(b'\n') {
+        if let Ok(line) = maybe_line {
+            let mut n : i32 = 0;
+            for digit in &line[1..] {
+                n *= 10;
+                n += (*digit - '0' as u8) as i32;
+            }
+            let sign = line[0] as char;
+            match sign {
+                '+' => frequency += n,
+                '-' => frequency -= n,
+                _ => {}
+            }
+        }
+    }
+    println!("{}", frequency);
+
+    Ok(())
 }
+
+
