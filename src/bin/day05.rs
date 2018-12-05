f61eca77b869cb32a8d123c431adf2d32b4bfa72
saolsen <steve.olsen@gmail.com>
2018-12-05 11:31:30 -0500

    day 5 different algorithm

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,15 +1,13 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
-use std::str;
-
-use std::collections::HashSet;
 
 fn main() -> std::io::Result<()> {
     let mut input = vec![];
     BufReader::new(File::open("src/bin/day05.txt")?).read_to_end(&mut input)?;
     assert_eq!(input.len() % 2, 0);
 
+    let mut min_len = std::usize::MAX;
     for polymer in 65..91 {
         let mut units = input.clone();
         let mut new_units = vec![];
@@ -22,34 +20,22 @@ fn main() -> std::io::Result<()> {
         units = new_units;
         assert_eq!(units.len() % 2, 0);
 
-        let mut reactions = true;
-        while (reactions) {
-            reactions = false;
-            if units.len() < 2 {
-                break;
-            }
-
-            let mut next_units = vec![];
-            let mut i = 0;
-
-            while i < units.len() - 1 {
-                let a = units[i];
-                let b = units[i+1];
-                
-                if (a as i32 - b as i32).abs() == 32 {
-                    reactions = true;
-                    i += 2;
-                } else {
-                    next_units.push(a);
-                    i += 1;
+        let mut stack = vec![];
+        for c in units {
+            if let Some(top) = stack.last() {
+                if (*top as i32 - c as i32).abs() == 32 {
+                    stack.pop();
+                    continue;
                 }
             }
-            if i == units.len() - 1 {
-                next_units.push(units[i]);
-            }
-            units = next_units;
+            stack.push(c);
         }
-        println!("Reaction Without {}{}, length: {}", polymer as char, (polymer + 32u8) as char, units.len());
+
+        println!("Reaction Without {}{}, length: {}", polymer as char, (polymer + 32u8) as char, stack.len());
+        if stack.len() < min_len {
+            min_len = stack.len();
+        }
     }
+    assert_eq!(min_len, 6946);
     Ok(())
 }
